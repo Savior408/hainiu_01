8900785d3c67727bf3d323e4b25f1171fef867ae
hainiu <hainiu@example.com>
2024-11-23 19:10:57 +0800

    creat new file Demo.java

---
+++ Demo.java
@@ -0,0 +1,14 @@
+About
+No description, website, or topics provided.
+Resources
+ Readme
+ Activity
+Stars
+ 0 stars
+Watchers
+ 1 watching
+Forks
+ 0 forks
+Releases
+No releases published
+Create a new release
